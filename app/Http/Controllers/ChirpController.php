120927f42ddfd6b5e5226551ffb74cb22a4fcf0c
leozoilo <138543112+leozoilo@users.noreply.github.com>
2023-07-24 13:46:07 +0800

    initial commit
    initial commit

---
+++ app/Http/Controllers/ChirpController.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\View\View; 
+use App\Models\chirp;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response; 
+
+class ChirpController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(): View
+    {
+        return view('chirps.index', [
+            'chirps' => Chirp::with('user')->latest()->get(),
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request): RedirectResponse
+    {
+        $validated = $request->validate([
+            'message' => 'required|string|max:255',
+        ]);
+ 
+        $request->user()->chirps()->create($validated);
+ 
+        return redirect(route('chirps.index'));
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\chirp  $chirp
+     * @return \Illuminate\Http\Response
+     */
+    public function show(chirp $chirp)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\chirp  $chirp
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(chirp $chirp): View
+    {
+        $this->authorize('update', $chirp);
+ 
+        return view('chirps.edit', [
+            'chirp' => $chirp,
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\chirp  $chirp
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Chirp $chirp): RedirectResponse
+    {
+        $this->authorize('update', $chirp);
+ 
+        $validated = $request->validate([
+            'message' => 'required|string|max:255',
+        ]);
+ 
+        $chirp->update($validated);
+ 
+        return redirect(route('chirps.index'));
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\chirp  $chirp
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Chirp $chirp): RedirectResponse
+    {
+        $this->authorize('delete', $chirp);
+ 
+        $chirp->delete();
+ 
+        return redirect(route('chirps.index'));
+    }
+}
